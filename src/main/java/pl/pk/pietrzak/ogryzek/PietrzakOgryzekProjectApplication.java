f31905193606a0b64f7aad58d41b4ead76272bbd
Arkadiusz <ogryzek.arkadiusz@gmail.com>
2026-03-07 13:01:59 +0100

    Repaired ProjectRepository.java

--- src/main/java/pl/pk/pietrzak/ogryzek/PietrzakOgryzekProjectApplication.java
+++ src/main/java/pl/pk/pietrzak/ogryzek/PietrzakOgryzekProjectApplication.java
@@ -4,13 +4,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import javax.sql.DataSource;
 
 @SpringBootApplication
-@EntityScan("pl.pk.pietrzak.ogryzek.entity")
 @EnableJpaRepositories("pl.pk.pietrzak.ogryzek.entity")
 public class PietrzakOgryzekProjectApplication {
 
