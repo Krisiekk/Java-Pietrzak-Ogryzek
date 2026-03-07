ef2ea0e16904ebf401fcae096a9a9cb0d5523e76
Krzysztof Pietrzak <krzysztofpietrzakx98@gmail.com>
2026-03-07 09:35:59 +0100

    Initialize Spring Boot project with Maven, Java 17, Lombok, JPA and PostgreSQL

---
+++ src/main/java/pl/pk/pietrzak/ogryzek/PietrzakOgryzekProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.pk.pietrzak.ogryzek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PietrzakOgryzekProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PietrzakOgryzekProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/pk/pietrzak/ogryzek/PietrzakOgryzekProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pk.pietrzak.ogryzek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PietrzakOgryzekProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
