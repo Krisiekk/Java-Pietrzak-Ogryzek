aa55cdffb26dba7d899ee5cadcde07cfafd68b78
Arkadiusz <ogryzek.arkadiusz@gmail.com>
2026-03-07 12:28:35 +0100

    ProjectRepository and entities EntityScan/EnableJpaRepositories added

---
+++ src/main/java/pl/pk/pietrzak/ogryzek/entity/ProjectRepository.java
@@ -0,0 +1,6 @@
+package pl.pk.pietrzak.ogryzek.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+}
