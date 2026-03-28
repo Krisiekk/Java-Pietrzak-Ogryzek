b9cf404df777bad6f819a3bd78d21a5d518eac72
Arkadiusz <ogryzek.arkadiusz@gmail.com>
2026-03-28 14:57:09 +0100

    Swagger adnotations initially added

--- src/main/java/pl/pk/pietrzak/ogryzek/controller/TaskController.java
+++ src/main/java/pl/pk/pietrzak/ogryzek/controller/TaskController.java
@@ -1,5 +1,8 @@
 package pl.pk.pietrzak.ogryzek.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +14,7 @@ import pl.pk.pietrzak.ogryzek.entity.TaskRepository;
 
 @RestController
 @RequestMapping("/api/tasks")
+@Tag(name = "Zadania", description = "Endpointy do zarzadzania zadaniami")
 public class TaskController {
 
     private final TaskRepository taskRepository;
@@ -20,12 +24,16 @@ public class TaskController {
     }
 
     @GetMapping
+    @Operation(summary = "Pobierz wszystkie zadania", description = "Zwraca liste wszystkich zadan")
     public List<Task> getAllTasks() {
         return taskRepository.findAll();
     }
 
     @PostMapping
-    public Task createTask(@RequestBody Task task) {
+    @Operation(summary = "Utworz nowe zadanie", description = "Tworzy nowe zadanie na podstawie przekazanych danych")
+    public Task createTask(
+            @Parameter(description = "Dane zadania do utworzenia", required = true)
+            @RequestBody Task task) {
         return taskRepository.save(task);
     }
 }
--- src/main/java/pl/pk/pietrzak/ogryzek/controller/UserController.java
+++ src/main/java/pl/pk/pietrzak/ogryzek/controller/UserController.java
@@ -1,5 +1,8 @@
 package pl.pk.pietrzak.ogryzek.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +14,7 @@ import pl.pk.pietrzak.ogryzek.entity.Users;
 
 @RestController
 @RequestMapping("/api/users")
+@Tag(name = "Uzytkownicy", description = "Endpointy do zarzadzania uzytkownikami")
 public class UserController {
 
     private final UserRepository userRepository;
@@ -20,12 +24,16 @@ public class UserController {
     }
 
     @GetMapping
+    @Operation(summary = "Pobierz wszystkich uzytkownikow", description = "Zwraca liste wszystkich uzytkownikow")
     public List<Users> getAllUsers() {
         return userRepository.findAll();
     }
 
     @PostMapping
-    public Users createUser(@RequestBody Users user) {
+    @Operation(summary = "Utworz nowego uzytkownika", description = "Tworzy nowego uzytkownika na podstawie przekazanych danych")
+    public Users createUser(
+            @Parameter(description = "Dane uzytkownika do utworzenia", required = true)
+            @RequestBody Users user) {
         return userRepository.save(user);
     }
 }
